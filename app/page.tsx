710225ce4c92f06a94cad752a80b428fe48b540a
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-15 17:46:30 +0700

    Weekly revenue chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import { Card, Title, Text } from '@tremor/react';
 import { queryBuilder } from '../lib/planetscale';
-import { authOptions } from '../pages/api/auth/[...nextauth]';
-import { google } from 'googleapis';
-
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { getServerSession } from 'next-auth/next';
 
 import Search from './search';
 import UsersTable from './table';
+import getSalesData from './actions/getSalesData';
+import WeeklyBarChart from './weeklyBarChart';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic',
+  revalidate = 1800;
 
 export default async function IndexPage({
   searchParams
 }: {
   searchParams: { q: string };
 }) {
+  const { values: sales, error } = await getSalesData();
+
   const search = searchParams.q ?? '';
   const users = await queryBuilder
     .selectFrom('users')
@@ -25,53 +25,16 @@ export default async function IndexPage({
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
+      {sales && <WeeklyBarChart salesData={sales} />}
+      {/* <Title>Users</Title>
       <Text>
         A list of users retrieved from a MySQL database (PlanetScale).
       </Text>
       <Search />
-      <Card className="mt-6">
-        {/* @ts-expect-error Server Component */}
-        <UsersTable users={users} />
-      </Card>
+      <Card className="mt-6"> */}
+      {/* @ts-expddect-error Server Component */}
+      {/* <UsersTable users={users} />
+      </Card> */}
     </main>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  const allowedEmails = JSON.parse(
-    process.env.ALLOWED_EMAILS as string
-  ) as Array<string>;
-
-  const isEmailAllowed = session?.user?.email
-    ? allowedEmails.includes(session?.user?.email)
-    : false;
-
-  if (isEmailAllowed) {
-    // Signed in
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.JSON_SECRET as string),
-      scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-    const client = await auth.getClient();
-    const sheets = google.sheets({ version: 'v4', auth: client });
-    const {
-      data: { values }
-    } = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
-      range: `salesData!A2:I`
-    });
-
-    console.log(sheets, 'values');
-    return {
-      props: {
-        values
-      }
-    };
-  } else {
-    return {
-      props: { values: [] }
-    };
-  }
-};
---
+++ app/weeklyBarChart.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { Card, Title, BarChart, Subtitle } from '@tremor/react';
+import { SaleData } from './actions/getSalesData';
+import moment from 'moment';
+
+const dataFormatter = (number: number) => {
+  return Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    currencySign: 'accounting',
+    minimumFractionDigits: 0
+  })
+    .format(number)
+    .toString();
+};
+
+export default function WeeklyBarChart({
+  salesData
+}: {
+  salesData: Array<SaleData>;
+}) {
+  // const formattedSalesData = salesData.map(sale)
+  const yearData = salesData.filter((date) =>
+    moment(date[1]).isBetween(
+      moment().startOf('year'),
+      moment().endOf('year'),
+      undefined,
+      '[]'
+    )
+  );
+  const chartObj = yearData.reduce(
+    (obj, sale) => {
+      const weekNumber = moment(sale[1]).isoWeek();
+
+      const revenue = parseFloat(sale[5]) * parseFloat(sale[6]);
+
+      if (obj[weekNumber]) {
+        obj[weekNumber].revenue = obj[weekNumber].revenue + revenue;
+      } else {
+        obj[weekNumber] = { revenue };
+      }
+
+      return obj;
+    },
+    {} as {
+      [key: string]: {
+        revenue: number;
+      };
+    }
+  );
+
+  let chartData = [];
+  for (let i = 1; i <= moment().isoWeeksInYear(); i++) {
+    const weekLabel = `W ${i} '${moment().format('YY')}`;
+    if (chartObj[i]) {
+      chartData.push({
+        name: weekLabel,
+        'Total Revenue': chartObj[i].revenue,
+        weekNumber: i
+      });
+    } else {
+      chartData.push({
+        name: weekLabel,
+        'Total Revenue': 0,
+        weekNumber: i
+      });
+    }
+  }
+
+  chartData = chartData.sort((week1, week2) => {
+    if (week2.weekNumber > week1.weekNumber) {
+      return -1;
+    }
+    if (week1.weekNumber > week2.weekNumber) {
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log(chartData);
+
+  return (
+    <Card>
+      <Title>Weekly Revenue</Title>
+      <Subtitle>
+        2023&apos;s weekly revenue (data from 13th of February).
+      </Subtitle>
+      <BarChart
+        className="mt-6"
+        data={chartData}
+        index="name"
+        categories={['Total Revenue']}
+        colors={['green']}
+        valueFormatter={dataFormatter}
+        yAxisWidth={80}
+      />
+    </Card>
+  );
+}
