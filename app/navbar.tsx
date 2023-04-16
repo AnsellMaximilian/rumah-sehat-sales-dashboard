5b46307be138b477a62dd59a32a3fff858e25e9c
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-16 09:21:35 +0700

    Change navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -37,17 +37,12 @@ export default function Navbar({ user }: { user: any }) {
                     className="text-gray-100"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <rect
-                      width="100%"
-                      height="100%"
-                      rx="16"
-                      fill="currentColor"
-                    />
+                    <rect width="100%" height="100%" rx="16" fill="#45f222" />
                     <path
                       fillRule="evenodd"
                       clipRule="evenodd"
                       d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                      fill="black"
+                      fill="#15803d"
                     />
                   </svg>
                 </div>
