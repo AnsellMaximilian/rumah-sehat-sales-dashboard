dd704e8959842fc5271e0b4d949ae5a5156a5d42
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-20 10:11:41 +0700

    Fix select

--- app/select.tsx
+++ app/select.tsx
@@ -41,7 +41,7 @@ export default function Select({
       <label htmlFor="search" className="sr-only">
         Select
       </label>
-      <div className="rounded-md shadow-sm">
+      <div>
         <div className="flex items-end justify-between">
           <label
             htmlFor="customer"
@@ -74,20 +74,31 @@ export default function Select({
             </div>
           )}
         </div>
-        <select
-          name="customer"
-          id="customer"
-          disabled={disabled}
-          onChange={(e) => handleSelect(e.target.value)}
-          className="h-10 block w-full rounded-md border border-gray-200 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        >
-          <option value="all">All</option>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
+        <div className="relative">
+          <select
+            name="customer"
+            id="customer"
+            disabled={disabled}
+            onChange={(e) => handleSelect(e.target.value)}
+            className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          >
+            <option value="all">All</option>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
